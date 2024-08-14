5b201b3e17d6d917b3b888ec1c26146c156e8f78
Eliana <eliana.ochoa@linktic.com>
2024-08-14 11:15:45 -0500

    Primer proyecto

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>	
+	import '../app.css';
+	import '../styles.css';
+	let src = '../src/lib/images/image-qr-code.png'
+
+</script>
+
+
+<body class="bg-indigo-200 flex items-center justify-center m-10">
+
+    <div class="container bg-slate-50 flex-col w-80 p-4 rounded-xl shadow-2xl">
+		<!--imagen codigo central-->
+        <div class="flex items-center justify-center">
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <img {src} class="rounded-xl" />              
+        </div>  
+		<!--subtitulo y texto-->
+        <div class="text-center py-6">
+            <h1 class="font-bold">Improve your front-end skills by building projects</h1>
+            <p class="text-gray-400">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>                
+        </div>
+    </div>
+</body>
+
